055f6f29ed5fe89c77767e8fb7f72808fde49cef
jlee2843 <jenny.jeeun@gmail.com>
2024-04-04 20:58:39 -0700

    added filters to prediction page

--- pages/prediction.py
+++ pages/prediction.py
@@ -5,52 +5,167 @@ from dash import html
 import dash_bootstrap_components as dbc
 import menu
 import dash
+from dash import dcc
+from dash.dependencies import Input, Output, State
+from app import app
+from dash import Dash, html, dcc, dash_table, ctx, callback
+from dash import callback_context
+import numpy as np
 
-dash.register_page(__name__, path="/", title="Observation")
+# from functions.visualization import map_fig
 
-SIDEBAR_STYLE = {
-    "top": 42,
-    "left": 0,
-    "bottom": 0,
-    "background-color": "#f8f9fa",
-    "overflowY": "auto",
-}
+import plotly.graph_objects as go
 
-sidebar = html.Div(
-    [
-        html.H3("Filter by:"),
+import pandas as pd
+dash.register_page(__name__, path="/", title="Prediction")
+
+df = pd.read_csv("data/raw/listings.csv")
+df = df[df["host_location"] == "Vancouver, Canada"]
+df.dropna(subset=['host_location', 'price', 'bathrooms_text'], inplace=True)
+df = df[["neighbourhood_cleansed", "accommodates", "price", "room_type", "beds", "bathrooms_text", "quarter"]]
+
+df["price_adjusted"] = df["price"].str.extract(r'([0-9.]+)', expand = False).astype(float)
+df["bathroom_adjusted"] = df["bathrooms_text"].str.extract(r'([0-9.]+)', expand = False).astype(float)
+
+sidebar = html.Div([
+        html.H4("Select Options", style={"margin-left": "14px"}),
         html.Hr(),
-        html.P(
-            "Dropdowns go here"
-        ),
-        # content
-    ],
-    style=SIDEBAR_STYLE,
+        dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                    html.Label("Select Longitude:", style={"color": "black"}),
+                    dbc.Input(id="longitude_input", placeholder="Enter in data type float", type="float", style={"margin-bottom": "20px"}),
+                ])
+            ]),
+
+              dbc.Row([
+                    dbc.Col([
+                          html.Label("Select Latitude:", style={"color": "black"}),
+                          dbc.Input(id="latitude_input", placeholder="Enter in data type float", type="float", style={"margin-bottom": "20px"})
+                    ])
+              ]),
+
+              dbc.Row([
+                    dbc.Col([
+                          html.Label("Number of Guests:", style={"color": "black"}),
+                          dcc.Dropdown(id="people_dropdown_eval",
+                                       options=np.arange(start=1, stop=10, step=1).tolist(),
+                                       multi=False,
+                                       style={"margin-bottom": "20px"})
+                    ])
+              ]),
+
+              dbc.Row([
+                    dbc.Col([
+                          html.Label("Room Type:", style={"color": "black"}),
+                          dcc.Dropdown(id="roomtype_dropdown_eval",
+                                       options=[{"label": r, "value": r} for r in df["room_type"].unique().tolist()],
+                                       multi=False,
+                                       style={"margin-bottom": "20px"})
+                    ])
+              ]),
+
+              dbc.Row([
+                    dbc.Col([
+                          html.Label("Number of Beds:", style={"color": "black"}),
+                          dcc.Dropdown(id="num_beds_dropdown_eval",
+                                       options=np.arange(start=1, stop=10, step=1).tolist(),
+                                       multi=False,
+                                       style={"margin-bottom": "20px"})
+                    ])
+              ]),
+
+              dbc.Row([
+                    dbc.Col([
+                          html.Label("Number of Bathrooms:", style={"color": "black"}),
+                          dcc.Dropdown(id="num_bathrooms_dropdown_eval",
+                                       options=np.arange(start=0.5, stop=6, step=0.5).tolist(),
+                                       multi=False,
+                                       style={"margin-bottom": "20px"})
+                    ])
+              ]),
+
+            dbc.Row([
+                  dbc.Col([
+                        dbc.ButtonGroup([
+                              dbc.Button("Get Evaluation", id="eval_button", n_clicks=0, style={'textAlign':'center'})])
+                  ],  align="center", style={'display': 'flex', 'justify-content': 'center'})
+            ], justify="center")
+
+        ])
+])
+
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        dbc.Row([
+            dbc.Col([
+                dash_table.DataTable(
+                        style_table={'overflowX': 'auto'},
+                        id="filtered_df",
+                        data=df.to_dict("records"),
+                        columns=[{'id': c, 'name': c} for c in df.columns],
+                        page_size=10,
+                        style_cell_conditional=[
+                            {
+                                'if': {'column_id': c},
+                                'textAlign': 'left'
+                            } for c in ['Date', 'Region']
+                        ],
+                        style_as_list_view=True,
+                        editable=True,
+                        sort_action="native",
+                        style_header={"backgroundColor": "#d85e30",
+                                      "fontweight": "bold", "color": "black",
+                                      "font_size": "14px"},
+                        style_cell={"font_family": "arial",
+                                    "font_size": "12px",
+                                    "text_align": "left"},
+                        style_data={'backgroundColor': 'transparent'},
+                        sort_mode="single")
+            ])
+        ])
+    ), style={"margin-top":"10px"}
+)
+
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        dbc.Row([
+            dbc.Col([
+                # dcc.Graph(figure=fig)
+            ])
+        ])
+    ), className="mt-3"
 )
 
 
 maindiv = html.Div(
     id="first-div",
     children=[
-        html.H1("Observation", style={"color": "#89CFF0",
-                                "margin-bottom": "10px"}),
-        # first row
         html.Div([
-            html.H2("Map"),
-            html.Hr(),
-            html.P(
-                "This is where the map goes", className="lead"
-            )
+            html.H4("Predicted Price per Night (CAD)"),
+        html.Hr(),
+        dbc.Card([
+        dbc.CardHeader(
+            dbc.Tabs([
+                html.P(id="eval_paragraph")
+                ])
+        ),
+        ], className="mt-3",
+        style={"margin-left": "20px",
+               "margin-right": "20px",
+               "margin-bottom": "30px",
+               "width":"auto"}),
         ]),
 
-        # second row
         html.Div([
-            html.H2("Summary Statistics"),
+            html.H4("Summary Statistics"),
             html.Hr(),
             html.P(
-                "This is where summary statistics go", className="lead"
+                "This is where summary statistics go"
             )
-        ]),
+        ], style={"margin-bottom": "30px",
+                  "width":"auto"})
+
     ]
 )
 
@@ -62,3 +177,22 @@ layout = html.Div(children=[
         dbc.Col(maindiv, width=9)  
     ])
 ])
+
+@app.callback(
+    Output('eval_paragraph', 'children'),
+    [Input('eval_button', 'n_clicks'),
+     State("people_dropdown_eval", "value"),
+     State("roomtype_dropdown_eval", "value"),
+     State("num_beds_dropdown_eval", "value"),
+     State("num_bathrooms_dropdown_eval", "value"),
+     State("longitude_input", "value"),
+     State("latitude_input", "value")]
+)
+def getOptionValues(eval_button, people_ddropdown_eval, 
+                    roomtype_dropdown_eval, 
+                    num_beds_dropdown_eval, 
+                    num_bathrooms_dropdown_eval,
+                    latitude_input, longitude_input):
+    if "eval_button" == ctx.triggered_id:
+        print(type(latitude_input))
+        print(latitude_input, longitude_input, roomtype_dropdown_eval, num_beds_dropdown_eval, people_ddropdown_eval, num_bathrooms_dropdown_eval)
