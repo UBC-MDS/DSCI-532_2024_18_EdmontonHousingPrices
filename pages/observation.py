152eb86865a9f6da79fb6119ba2e2093af629851
ella-irene <ellaihein@gmail.com>
2024-04-11 01:02:55 -0700

    fixed color and margin

--- pages/observation.py
+++ pages/observation.py
@@ -379,19 +379,32 @@ def get_location(neighbourhood_dropdown,
     
     fig = go.Figure(go.Scattermapbox(
         lat=df_filtered["latitude"],
-        lon=df_filtered["longitude"],
+        lon=df_filtered["longitude"], 
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=9
-        ),
-        text=df_filtered["neighbourhood_cleansed"]
+            size=10,
+            opacity = 0.7,
+            color = df_filtered["price_adjusted"],
+            showscale=True,
+            colorscale='Peach'
+        )
+      #  text=[df_filtered["price_adjusted"], df_filtered["beds"]],
+      #  hoverinfo='text'
+        
     ))
 
+   # fig.update_traces(hovertemplate='Price: %{price_adjusted} <br>Accomodates: %{accommodates}')
+
+
     fig.update_layout(
+        margin={"r":0,"t":0,"l":0,"b":0},
         mapbox_style="carto-positron",
         mapbox_zoom=10,
         mapbox_center={"lat": df_filtered["latitude"].mean(), "lon": df_filtered["longitude"].mean()}
     )
+
+   
+
     avg_accom = [
         dbc.CardHeader('Average Number of Accomodates (Guests)', style={"text-align": "center"}),
         dbc.CardBody(f'{df_filtered["accommodates"].mean() :.1f}', style={"text-align": "center"})
